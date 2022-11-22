f6d5de494637dc00895de91ef6f5eb56cbf7e288
Pedro Seibel <pedrosbl@icloud.com>
2022-11-22 14:59:53 -0300

    Product component + showProducts function.

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,6 +5,7 @@ import Landing from "../components/Landing";
 import { Tab } from "@headlessui/react";
 import { fetchCategories } from "../utils/fetchCategories";
 import { fetchProducts } from "../utils/fetchProducts";
+import Product from "../components/Product";
 
 interface Props {
   categories: Category[];
@@ -12,6 +13,12 @@ interface Props {
 }
 
 const Home = ({ categories, products }: Props) => {
+  const showProducts = (category: number) => {
+    return products
+      .filter((product) => product.category._ref === categories[category]._id)
+      .map((product) => <Product product={product} key={product._id}/>); //Filter products by category
+  };
+
   return (
     <div className="">
       <Head>
@@ -50,10 +57,10 @@ const Home = ({ categories, products }: Props) => {
               ))}
             </Tab.List>
             <Tab.Panels className="mx-auto max-w-fit pt-10 pb-24 sm:px-4">
-              {/* <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className="tabPanel">{showProducts(0)}</Tab.Panel>
               <Tab.Panel className="tabPanel">{showProducts(1)}</Tab.Panel>
               <Tab.Panel className="tabPanel">{showProducts(2)}</Tab.Panel>
-              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel> */}
+              <Tab.Panel className="tabPanel">{showProducts(3)}</Tab.Panel>
             </Tab.Panels>
           </Tab.Group>
         </div>
