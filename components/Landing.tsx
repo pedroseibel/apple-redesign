b106c47d4b381ce0752bab76e5b792efd33ffe75
Pedro Seibel <pedrosbl@icloud.com>
2023-01-18 19:23:26 -0300

    Changing hero section slogan

--- components/Landing.tsx
+++ components/Landing.tsx
@@ -7,10 +7,10 @@ function Landing() {
       <div className="space-y-8">
         <h1 className="space-y-3 text-5xl font-semibold tracking-wide lg:text-6xl xl:text-7xl">
           <span className="block bg-gradient-to-r from-pink-500 to-violet-500 bg-clip-text text-transparent">
-            Lorem
+            Powered
           </span>
-          <span className="block">ipsum dolor</span>
-          <span className="block">sit amet</span>
+          <span className="block">By Intellect</span>
+          <span className="block">Driven By Values</span>
         </h1>
 
         <div className="space-x-8">
